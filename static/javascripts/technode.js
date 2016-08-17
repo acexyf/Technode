c2e59edadca114aaf5874a11e9cf38492e5b13cc
研发中心-谢余飞17469 <xyf17469@tcent.cn>
2016-08-17 10:21:49 +0800

    更新

---
+++ static/javascripts/technode.js
@@ -0,0 +1,29 @@
+// var socket=io.connect('/');
+// socket.on('connected',function(){
+// 	alert('connect to server');
+// });
+
+
+angular.module('technodeApp').factory('socket',function($rootScope){
+	var socket=io.connect('/');
+	return {
+		on:function(eventName,callback){
+			socket.on(eventName,function(){
+				var args=arguments;
+				$rootScope.$apply(function(){
+					callback.apply(socket,args);
+				});
+			});
+		},
+		emit:function(eventName,data,callback){
+			socket.emit(eventName,data,function(){
+				var args=arguments;
+				$rootScope.$apply(function(){
+					if(callback){
+						callback.apply(socket,args);
+					}
+				});
+			});
+		}
+	}
+});
